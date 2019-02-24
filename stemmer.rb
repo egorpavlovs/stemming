7acb60a73851b903e8296d07dcf9f90d1b198576
Egor Pavlov <pavlov_egor007@mail.ru>
2019-02-24 20:49:35 +0300

    Regenerate stemming docs, small fixes in Stemmer

--- stemmer.rb
+++ stemmer.rb
@@ -11,19 +11,21 @@ class Stemmer
       all_paths = Dir["#{DOCS_HOME_PATH}/*/*/"]
       words = all_paths.map do |path|
         content = File.open([path, "content.txt"].join("/"), "r"){ |f| f.read }
-        tokenize_words = tokenize(content).map { |word| Lingua.stemmer(word) unless word.nil? }.compact
+        stop_words = File.open("stop_words", "r"){ |f| f.read }.split("\n")
+
+        tokenize_words = tokenize(content, stop_words).map { |word| Lingua.stemmer(word) unless word.nil? }.compact
         s_path = FileUtils.mkdir_p([S_DOCS_HOME_PATH, path.split("/").drop(1)].flatten.join("/"))
         puts s_path
-        tokenize_words.each{ |word|
+        tokenize_words.uniq.each{ |word|
           File.open([s_path, "content.txt"].flatten.join("/"), "a") {|file| file.puts(word)}
         }
       end
     end
 
-    def tokenize(content)
+    def tokenize(content, stop_words)
       numbers = content.scan(/\d+/)
       result = content.split(/\W+/) - numbers
-      result.map{ |word| word if word.size > 2 && word.gsub(/\d+/,"") == word}
+      result.map{ |word| word.downcase if word.size > 1 && word.gsub(/\d+/,"") == word && !stop_words.include?(word) }
     end
 
   end
