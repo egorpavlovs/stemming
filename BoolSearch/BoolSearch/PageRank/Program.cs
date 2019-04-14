ae3c627fb8c1a76873ab7e9425b70c0752946138
andreyandrianov <andrey.andrianov2011@ya.ru>
2019-04-14 23:39:43 +0300

    Added page rank

--- BoolSearch/BoolSearch/PageRank/Program.cs
+++ BoolSearch/BoolSearch/PageRank/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace PageRank
+﻿using System;
+
+namespace PageRank
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PageRank pr = new PageRank();
+            pr.CalculatePageRanks();
+            Console.ReadLine();
         }
     }
 }
